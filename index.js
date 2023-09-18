a56f04bd85512a87bc01810e20fc873c321355d4
marielheredia <139135835+marielheredia@users.noreply.github.com>
2023-09-18 12:21:59 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let nombreingresado = prompt ("Porfavor, ingrese su nombre");
+if (nombre !==null&&nombre!=="") 
+{
+alert ("¡Bienvenida!  "   + nombreingresado );
+  console.log ( "Nombre del usuarios: " + nombre "!Biendenidos a nuestro sitio web!.");
+}
+else { "No proporcionaste un nombre");
